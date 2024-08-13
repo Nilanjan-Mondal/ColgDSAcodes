05e5be29858d0d1416b9849e99c0792be72dc6c9
UEM <nilanjanmondal165@gmail.com>
2024-08-13 12:32:26 +0530

    Added delete functions

--- LINKED LIST/LLclass.py
+++ LINKED LIST/LLclass.py
@@ -46,26 +46,75 @@ class Linked_List:
         newNode.next = temp.next
         temp.next = newNode
 
+    def deleteFromHead(self):
+        if self.head is None:
+            print("The list is empty.")
+            return
+        self.head = self.head.next
+
+    def deleteFromTail(self):
+        if self.head is None:
+            print("The list is empty.")
+            return
+        if self.head.next is None:
+            self.head = None
+            return
+        temp = self.head
+        while temp.next.next is not None:
+            temp = temp.next
+        temp.next = None
+
+    def deleteAtAny(self, pos):
+        if self.head is None:
+            print("The list is empty.")
+            return
+        if pos == 1:
+            self.head = self.head.next
+            return
+        temp = self.head
+        for _ in range(pos - 2):
+            if temp.next is None:
+                print("The position is out of range.")
+                return
+            temp = temp.next
+        if temp.next is None:
+            print("The position is out of range.")
+            return
+        temp.next = temp.next.next  
+
     def display(self):
         temp = self.head
         while(temp is not None):
             print(temp.val, end = " ")
             temp = temp.next
 
+
 ll = Linked_List()
 ll.createLL()
+
 val = int(input("Enter the value to be inserted at head: "))   
 ll.insertAtHead(val)
 ll.display()
-print("\n")
+
 val = int(input("Enter the value to be inserted at tail: "))   
 ll.insertAtTail(val)
-ll.display() 
-print("\n")
+ll.display()
+
 val = int(input("Enter the value to be inserted at any location: ")) 
 pos = int(input("Enter the position: "))
-ll.insertAtAny(val,pos)
-ll.display() 
+ll.insertAtAny(val, pos)
+ll.display()
 
+print("\nDeleting from head...")
+ll.deleteFromHead()
+ll.display()
+
+print("\nDeleting from tail...")
+ll.deleteFromTail()
+ll.display()
+
+pos = int(input("Enter the position to delete: "))
+ll.deleteAtAny(pos)
+ll.display()
 
 
